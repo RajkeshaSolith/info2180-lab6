0ae97b325407db1f2f1ca9c689180e19e8831d5c
RajkeshaSolith <sajonelle@gmail.com>
2019-11-11 20:10:13 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
 window.onload = function(){
 
-	//var loadBtn = document.
-}
+	var loadBtn = document.querySelector('#loadBtn');
+	var httpRequest;
+
+	loadBtn.addEventListener('click', function(element) {
+    element.preventDefault();
+
+    httpRequest = new XMLHttpRequest();
+
+    // GET Request
+    var url = "superheroes.php";
+    httpRequest.onreadystatechange = loadSearch;
+    httpRequest.open('GET', url);
+    httpRequest.send();
+	});
+
+	function loadSearch() {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+      if (httpRequest.status === 200) {
+        var response = httpRequest.responseText;
+        var demo = document.querySelector('#demo');
+        demo.innerHTML = response;
+      } else {
+        alert('There was a problem with the request.');
+      }
+    }
+  }
+
+
+};
